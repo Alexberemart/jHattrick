4dc3df429fa62a459cf9578b7830f5ba4b7885f2
Alejandro Berenguer <alexberemart@gmail.com>
2015-09-16 10:00:09 +0200

    Create TeamDetails Pojo

---
+++ src/main/java/alexberemart/jHattrick/model/vo/HtTeamDetail/HtTeamDetailRegion.java
@@ -0,0 +1,23 @@
+package alexberemart.jHattrick.model.vo.HtTeamDetail;
+
+public class HtTeamDetailRegion {
+
+    protected Integer regionId;
+    protected String regionName;
+
+    public Integer getRegionId() {
+        return regionId;
+    }
+
+    public void setRegionId(Integer regionId) {
+        this.regionId = regionId;
+    }
+
+    public String getRegionName() {
+        return regionName;
+    }
+
+    public void setRegionName(String regionName) {
+        this.regionName = regionName;
+    }
+}
---
+++ src/main/java/alexberemart/jHattrick/model/vo/HtTeamDetail/HtTeamDetailTrainer.java
@@ -0,0 +1,14 @@
+package alexberemart.jHattrick.model.vo.HtTeamDetail;
+
+public class HtTeamDetailTrainer {
+
+    protected Integer playerID;
+
+    public Integer getPlayerID() {
+        return playerID;
+    }
+
+    public void setPlayerID(Integer playerID) {
+        this.playerID = playerID;
+    }
+}
