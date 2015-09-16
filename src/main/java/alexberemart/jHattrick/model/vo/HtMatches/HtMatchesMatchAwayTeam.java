f572f3ab66b1b8fc39cada27ba335e79a8a2a854
Alejandro Berenguer <alexberemart@gmail.com>
2015-09-16 09:59:49 +0200

    Create HtMatches Pojo

---
+++ src/main/java/alexberemart/jHattrick/model/vo/HtMatches/HtMatchesMatchAwayTeam.java
@@ -0,0 +1,23 @@
+package alexberemart.jHattrick.model.vo.HtMatches;
+
+public class HtMatchesMatchAwayTeam {
+
+    protected Integer awayTeamId;
+    protected String awayTeamName;
+
+    public Integer getAwayTeamId() {
+        return awayTeamId;
+    }
+
+    public void setAwayTeamId(Integer awayTeamId) {
+        this.awayTeamId = awayTeamId;
+    }
+
+    public String getAwayTeamName() {
+        return awayTeamName;
+    }
+
+    public void setAwayTeamName(String awayTeamName) {
+        this.awayTeamName = awayTeamName;
+    }
+}
---
+++ src/main/java/alexberemart/jHattrick/model/vo/HtMatches/HtMatchesMatchHomeTeam.java
@@ -0,0 +1,23 @@
+package alexberemart.jHattrick.model.vo.HtMatches;
+
+public class HtMatchesMatchHomeTeam {
+
+    protected Integer homeTeamId;
+    protected String homeTeamName;
+
+    public Integer getHomeTeamId() {
+        return homeTeamId;
+    }
+
+    public void setHomeTeamId(Integer homeTeamId) {
+        this.homeTeamId = homeTeamId;
+    }
+
+    public String getHomeTeamName() {
+        return homeTeamName;
+    }
+
+    public void setHomeTeamName(String homeTeamName) {
+        this.homeTeamName = homeTeamName;
+    }
+}
